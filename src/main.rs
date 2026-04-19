299af34576a6c3ac19db5945f92c7a12ca19072d
alexander <alexandersznunes@gmail.com>
2026-04-19 12:55:48 -0300

    Generate HTTP response with dynamic headers

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,32 @@ use std::{
     net::{SocketAddr, TcpListener, TcpStream},
 };
 
-
-pub struct HTTPRequest {
-    method: String,
-    uri: String,
-    http_version: String,
-    header_fields: HashMap<String, String>,
-}
+use chrono::Utc;
+mod http;
+use http::request::HTTPRequest;
 
 fn main() -> std::io::Result<()> {
+    const SERVER_NAME: &str = "HTTPServer/0.0.1";
     let addrs = SocketAddr::from(([127, 0, 0, 1], 7878));
 
     let listener = TcpListener::bind(&addrs)?;
     let mut response: Vec<u8> = Vec::new();
-    let response_header: &[u8] = "HTTP/1.1 200 OK\r\nDate: Mon, 27 Jul 2009 12:28:53 GMT\r\nServer: Apache/2.2.14 (Win32)\r\nLast-Modified: Wed, 22 Jul 2009 19:15:56 GMT\r\nContent-Length: 4751\r\nContent-Type: text/html\r\nConnection: Closed\r\n\r\n"
-        .as_bytes();
-    response.extend_from_slice(response_header);
+
+    //datetime
+    let utc_time = Utc::now().format("%a, %d %b %Y %H:%M:%S %Z");
+
     let mut file = File::open("src/content/HelloWorld.html").unwrap();
     let mut response_content: Vec<u8> = Vec::new();
-    file.read_to_end(&mut response_content).unwrap();
+    let content_length: usize = file.read_to_end(&mut response_content).unwrap();
+    let response_header_f: String = format!(
+        "HTTP/1.1 200 OK\r\nDate: {utc_time}\r\nServer: {SERVER_NAME}\r\nLast-Modified: Wed, 22 Jul 2009 19:15:56 GMT\r\nContent-Length: {content_length}\r\nContent-Type: text/html\r\nConnection: Closed\r\n\r\n"
+    );
+    print!("{:#?}",file.metadata());
+    let response_header_b: &[u8] = response_header_f.as_bytes();
+    response.extend_from_slice(response_header_b);
     response.extend_from_slice(&response_content);
 
-    println!("\n- SocketAddr: {:#?}", &addrs);
+    println!("\n- Address: http://{:#?}\n", &addrs);
 
     for stream in listener.incoming() {
         handle_conncection(stream?, response.as_slice())?;
@@ -67,7 +71,8 @@ fn handle_conncection(stream: TcpStream, response_bytes: &[u8]) -> std::io::Resu
     for fields in &headers {
         let line = fields
             .split_once(':')
-            .map(|elem| {(elem.0.trim(),elem.1.trim())}).unwrap_or(("",""));
+            .map(|elem| (elem.0.trim(), elem.1.trim()))
+            .unwrap_or(("", ""));
 
         header_fields.insert(line.0.to_string(), line.1.to_string());
     }
@@ -87,15 +92,12 @@ fn handle_conncection(stream: TcpStream, response_bytes: &[u8]) -> std::io::Resu
         uri: req_line_parts[1].to_owned(),
         http_version: req_line_parts[2].to_owned(),
         header_fields: header_fields,
+        body: body.to_string(),
     };
 
     println!(
-        "Request Method: {}, URI: {}, HTTPVersion: {}\nRequest Header: {:#?}\nRequest Body: {:#?}",
-        http_req.method,
-        http_req.uri,
-        http_req.http_version,
-        http_req.header_fields,
-        body.as_ref()
+        "Request Method: {:#?}, URI: {:#?}, HTTPVersion: {:#?}\nRequest Header: {:#?}\nRequest Body: {:#?}",
+        http_req.method, http_req.uri, http_req.http_version, http_req.header_fields, http_req.body,
     );
 
     // envio de reposta apos leitura de requisicao
